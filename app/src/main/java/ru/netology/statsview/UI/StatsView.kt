7eb043429bd90b33188dcfc57ea4e2ed9175242d
Anisimova Irina <irstys@yandex.ru>
2022-12-18 12:25:10 +0300

    2.2. Animations — создание анимаций. Часть 1

--- app/src/main/java/ru/netology/statsview/UI/StatsView.kt
+++ app/src/main/java/ru/netology/statsview/UI/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.statsview.ui
 
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
@@ -14,6 +15,10 @@ import ru.netology.statsview.R
 import ru.netology.statsview.R.color.empty_color
 import kotlin.math.min
 import kotlin.random.Random
+
+const val TYPE_ROTATION = 0
+const val TYPE_SEQUENTIAL = 1
+const val TYPE_BIDIRECTIONAL = 2
 
 class StatsView @JvmOverloads constructor(
     context: Context,
@@ -31,12 +36,22 @@ class StatsView @JvmOverloads constructor(
     private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
     private var colors = emptyList<Int>()
 
+    private var progress = 0F
+
+    private var startFrom = -90F
+
+    private var animationType = 0
+    private var valueAnimator: ValueAnimator? = null
+
+    private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+
     init {
         context.withStyledAttributes(attrs, R.styleable.StatsView) {
             lineWidth = getDimension(R.styleable.StatsView_lineWidth, lineWidth)
             fontSize = getDimension(R.styleable.StatsView_fontSize, fontSize)
             val resId = getResourceId(R.styleable.StatsView_colors, 0)
             colors = resources.getIntArray(resId).toList()
+            animationType = getInteger(R.styleable.StatsView_animationType, 0)
         }
     }
 
@@ -67,8 +82,12 @@ class StatsView @JvmOverloads constructor(
     var data: List<Float> = emptyList()
         set(value) {
             field = value
-            invalidate()
+            update()
+        //invalidate()
         }
+
+    private val indexDate = data.maxOrNull()!!.times(data.count())
+    private fun getAngle(datum:Float) = (datum / indexDate) * fullCircleDegrees
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         radius = min(w, h) / 2F - lineWidth / 2
@@ -78,37 +97,110 @@ class StatsView @JvmOverloads constructor(
             center.x + radius, center.y + radius,
         )
     }
-    private val indexDate=data.maxOrNull()!!.times(data.count())
+
     override fun onDraw(canvas: Canvas) {
         if (data.isEmpty()) {
             return
         }
-
-        var startFrom = -90F
         canvas.drawArc(oval, startFrom, fullCircleDegrees, false, paintEmpty)
-        /*  for ((index, datum) in data.withIndex()) {
-            val angle = 360F * datum
-         }*/
-        data.forEachIndexed { index, datum ->
-            val angle = (datum / indexDate) * fullCircleDegrees
-            paint.color = colors.getOrElse(index) { randomColor() }
-            canvas.drawArc(oval, startFrom, angle, false, paint)
-            startFrom += angle
+
+        when (animationType) {
+            TYPE_ROTATION -> {
+                rotate(canvas)
+            }
+            TYPE_SEQUENTIAL -> {
+                sequent(canvas)
+            }
+            TYPE_BIDIRECTIONAL -> {
+                bidirectional(canvas)
+            }
         }
-
-
         val text = (data.sum() / indexDate) * 100F
         canvas.drawText(
-            "%.2f%%".format(text),
+            "%.2f%%".format(text + progress),
             center.x,
             center.y + textPaint.textSize / 4,
-            textPaint,
+            textPaint
         )
-        if (text == 100F) {
+        if (text==100F)
             paint.color = colors[0]
             canvas.drawArc(oval, startFrom, 1F, false, paint)
+    }
+
+    private fun update() {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 3000
+            start()
+            invalidate()
+            }
+    }
+    private fun rotate(canvas: Canvas) {
+
+        data.forEachIndexed { index, datum ->
+            val angle = getAngle(datum)
+
+            paint.color = colors.getOrElse(index) { randomColor() }
+            canvas.drawArc(oval, startFrom, angle * progress, false, paint)
+            startFrom += angle
+            if (index == 0 && progress == 1F) {
+                paint.color = colors[0]
+                canvas.drawPoint(center.x ,center.y - radius, paint)
+            }
         }
     }
 
-    private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
+    private fun sequent(canvas: Canvas) {
+
+        var sum = 0F
+
+        for ((index, datum) in data.withIndex()) {
+            val angle = getAngle(datum)
+            paint.color = colors.getOrElse(index) { randomColor() }
+
+            if(progress > datum + sum){
+                canvas.drawArc(oval, startFrom ,angle,false,paint)
+            }else if(progress in sum..(datum + sum)){
+                canvas.drawArc(oval, startFrom ,360 * (progress - sum),false,paint)
+            }
+            sum += datum
+            startFrom += angle
+
+            if (index == 0 && progress == 1F) {
+                paint.color = colors[0]
+                canvas.drawPoint(center.x ,center.y - radius, paint)
+            }
+        }
+    }
+
+    private fun bidirectional(canvas: Canvas) {
+
+        startFrom = -45F
+        data.forEachIndexed { index, datum ->
+            val angle = getAngle(datum)
+            paint.color = colors.getOrElse(index) { randomColor() }
+            canvas.drawArc(oval,
+                startFrom + (angle / 2) - (angle * progress / 2),
+                angle * progress,
+                false,
+                paint)
+            startFrom += angle
+            if (progress == 1F) {
+                paint.color = colors.get(0)
+                canvas.drawPoint(center.x,
+                    center.y - radius, paint)
+            }
+        }
+    }
 }
+
+
